c2eb7352c5e3d963f79b2aa622d58db407c7752c
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-02-21 11:40:55 -0600

    initial settings

--- src/config/database.js
+++ src/config/database.js
@@ -9,7 +9,7 @@ const configDatabase = {
   connectionString: process.env.DATABASE_URL,
 };
 
-if (process.env.MODE === "dev") configDatabase.ssl = true;
+if (process.env.MODE === "prod") configDatabase.ssl = true;
 
 
 const connection = new Pool(configDatabase);
