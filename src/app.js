29f7eb4195be674db08ca931e53596b468f14445
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-02-23 17:58:13 -0600

    fix: app mofification

--- src/app.js
+++ src/app.js
@@ -11,5 +11,5 @@ app.use(cors());
 
 app.use([usersRouter, urlShortenRouter]);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
